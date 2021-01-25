21e185d5a8a78a08fa2512f0171ac66ad0cf1e41
Amatullah Brown <amatullahbrown@Amatullahs-Air.fios-router.home>
2021-01-24 20:59:54 -0500

    implemented tests and class for ATM

--- src/ATM.js
+++ src/ATM.js
@@ -1,4 +1,42 @@
 
 class ATM {
-
+    constructor(amount, interestRate) {
+        if (isNaN(amount)) {
+            throw new TypeError('Amount must be numeric');
+        } else {
+            this.amount = parseFloat(amount);
+            // this.interestRate = amount;
+        }
+        //interest rate is put in as a percentage, if user wishes to put in interest as a whole number (eg 20% -> 20, would need to divide by 100)
+        if (isNaN(interestRate)) {
+            throw new TypeError('Amount must be numeric');
+        } else {
+            this.interestRate = parseFloat(interestRate);
+        }
+    }
+    withdraw(amount) {
+        if (isNaN(amount)) {
+            throw new TypeError('Amount must be numeric');
+        } else {
+            this.amount -= amount;
+        }
+        return this.amount;
+    }
+    deposit(amount) {
+        if (isNaN(amount)) {
+            throw new TypeError('Amount must be numeric');
+        } else {
+            this.amount += amount;
+        }
+        return this.amount;
+    }
+    applyInterest() {
+        this.amount += this.amount * this.interestRate;
+    }
+    getCurrentBalance() {
+        return this.amount;
+    }
 }
+
+module.exports = ATM;
+
